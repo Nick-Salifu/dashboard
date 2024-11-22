56d2906bdca51471a4ebfba2bd3303e046ebff92
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-11-22 21:48:11 +0100

    /dashboard commit

--- app/components/dashboard-component/transaction/transaction.jsx
+++ app/components/dashboard-component/transaction/transaction.jsx
@@ -1,7 +1,85 @@
+import Image from "next/image"
+import styles from "./transaction.module.css"
+
 export default function Transaction() {
     return (
-        <div>
-            <h2>This is the transaction section</h2>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Latest Transactions</h2>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Status</td>
+                        <td>Date</td>
+                        <td>Amount</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/user.jfif" alt="avatar" width={40} height={40} className={styles.userImage} />
+                                Nick
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>
+                                Pending
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$5.20</td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/user.jfif" alt="avatar" width={40} height={40} className={styles.userImage} />
+                                Nick
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.done}`}>
+                                Done
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$5.20</td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/user.jfif" alt="avatar" width={40} height={40} className={styles.userImage} />
+                                Nick
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.cancelled}`}>
+                                Cancelled
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$5.20</td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/user.jfif" alt="avatar" width={40} height={40} className={styles.userImage} />
+                                Nick
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>
+                                Pending
+                            </span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$5.20</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
